84f34c8f03991c794af0b37e4d04aee80d4fdf31
AdishaWaghare <87144330+AdishaWaghare@users.noreply.github.com>
2023-07-16 16:33:39 +0530

    first commit

---
+++ Javscript/ex.js
@@ -0,0 +1,14 @@
+document.write("Adisha<Br>");
+document.write("Adisha<Br>") ;
+    document.write("Adisha<Br>");
+        document.write("Adisha<Br>");
+            document.write("Adisha<Br>");
+                document.write("<h1>Adisha</h1>");
+                document.write("Adisha")
+
+                console.log("javascript");
+                console.log("javascript");
+                console.log("javascript");
+                console.log("javascript");
+                console.log("javascript");
+                console.log("<h1>javascript</h1>");
